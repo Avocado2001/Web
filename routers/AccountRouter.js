115c173c681256693354d7c988786c2bb7b4c86b
Avocado2001 <93489697+Avocado2001@users.noreply.github.com>
2022-05-10 15:48:33 +0700

    update

--- routers/AccountRouter.js
+++ routers/AccountRouter.js
@@ -4,6 +4,19 @@ const Account = require('../models/AccountModel');
 const registerValidator = require('./validators/registerValidator');
 const loginValidator = require('./validators/loginValidator');
 const { render } = require('express/lib/response');
+
+var generator = require('generate-password');
+//Tự tạo mật khẩu
+var password = generator.generate({
+    length: 6,
+    numbers: true
+});
+//Tạo username tự động(chưa so sánh username đã có hay chưa)
+var username = generator.generate({
+    length: 10,
+    numbers: true,
+    exclude: ''
+});
 Router.get('/', (req, res) => {
     res.json({
         code: 0,
