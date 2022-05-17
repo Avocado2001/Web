ddf11e1b957c1a60150a2b00e5b338918569b67a
Avocado2001 <93489697+Avocado2001@users.noreply.github.com>
2022-05-17 23:48:56 +0700

    update change pass

--- routers/validators/changePassValidator.js
+++ routers/validators/changePassValidator.js
@@ -1,5 +1,9 @@
 const { check } = require('express-validator');
 module.exports = [
+    check('oldpass')
+    .exists().withMessage('Password cũ không được để trống')
+    .notEmpty().withMessage('Password cũ không được để trống')
+    .isLength({ min: 6 }).withMessage('Password gồm 6 ký tự trở lên'),
     check('confirm1')
     .exists().withMessage('Password mới không được để trống')
     .notEmpty().withMessage('Password mới không được để trống')
