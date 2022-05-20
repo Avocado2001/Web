86d03391595a3760f8495a7468cab43c641ce474
banhtrangtron <tranminhkha12cb4tqt@gmail.com>
2022-05-20 16:59:29 +0700

    udpate

--- models/TransactionModel.js
+++ models/TransactionModel.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const today = new Date();
 const TransactionSchema = new Schema({
     username: String,
-    time: { type: Date, default: Date.now },
+    time: { type: Date, default: today.toLocaleString('ICT', {timeZone: 'Asia/ho_chi_minh'})},
     money: { type: Number, default: 0 },
  
     //chuyển tiền: 0, nạp tiền: 1, rút tiền: 2, mua thẻ: 3
