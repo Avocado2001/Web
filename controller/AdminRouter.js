aea0018f562537d3853da2a81a90c99aa8064c25
Avocado2001 <93489697+Avocado2001@users.noreply.github.com>
2022-05-29 15:24:28 +0700

    Update AdminRouter.js

--- controller/AdminRouter.js
+++ controller/AdminRouter.js
@@ -19,7 +19,7 @@ const transporter = nodemailer.createTransport({
 let userList = new Map();
 const Router = express.Router();
 Router.get('/', CheckLogin, (req, res) => {
-    Account.find({ isAdmin: false }, function (err, users) {
+    Account.find({ isAdmin: false }, function(err, users) {
         res.render('admin', {
             users
         });
@@ -29,7 +29,7 @@ Router.get('/', CheckLogin, (req, res) => {
 //danh sách chờ xác minh
 Router.get('/waitActive', CheckLogin, (req, res) => {
     var sort = { date_register: -1 };
-    Account.find({ isAdmin: false, $or: [{ status: 0 }, { status: 2 }] }, function (err, users) {
+    Account.find({ isAdmin: false, $or: [{ status: 0 }, { status: 2 }] }, function(err, users) {
         res.render('waitActive', {
             users
         });
@@ -40,7 +40,7 @@ Router.get('/waitActive', CheckLogin, (req, res) => {
 
 // Xem thông tin chi tiết chờ kích hoạt
 Router.get('/detailuser/:id', CheckLogin, (req, res) => {
-    Account.findById(req.params.id, function (err, user) {
+    Account.findById(req.params.id, function(err, user) {
         res.render('detailuser', {
             user
         });
@@ -49,12 +49,12 @@ Router.get('/detailuser/:id', CheckLogin, (req, res) => {
 
 // Xác minh tài khoản khóa do đăng nhập sai
 Router.post('/detailban/:id', CheckLogin, (req, res) => {
-    let {  inconstant_login,wrong_pass } = req.body;
-    inconstant_login = parseInt( inconstant_login);
-    if ( inconstant_login === 0) {
+    let { inconstant_login } = req.body;
+    inconstant_login = parseInt(inconstant_login);
+    if (inconstant_login === 0) {
         Account.findByIdAndUpdate(req.params.id, {
             inconstant_login,
-            wrong_pass:0
+            wrong_pass: 0
         }).then(() => {
             return res.redirect('/admin/detailban/' + req.params.id);
         });
@@ -88,8 +88,8 @@ Router.post('/detailuser/:id', CheckLogin, (req, res) => {
 });
 // Xem thông tin chi tiết tài khoản khóa do đăng nhập sai
 Router.get('/detailban/:id', CheckLogin, (req, res) => {
-   
-    Account.findById(req.params.id, function (err, user) {
+
+    Account.findById(req.params.id, function(err, user) {
         res.render('detailban', {
             user
         });
@@ -99,7 +99,7 @@ Router.get('/detailban/:id', CheckLogin, (req, res) => {
 //danh sách đã xác minh - sắp xếp giảm dần theo ngày tạo
 Router.get('/actived', CheckLogin, (req, res) => {
     var sort = { date_register: -1 };
-    Account.find({ isAdmin: false, status: 1 }, function (err, users) {
+    Account.find({ isAdmin: false, status: 1 }, function(err, users) {
         res.render('actived', {
             users
         });
@@ -110,7 +110,7 @@ Router.get('/actived', CheckLogin, (req, res) => {
 //danh sách bị vô hiệu hóa
 Router.get('/banning', CheckLogin, (req, res) => {
     var sort = { date_register: -1 };
-    Account.find({ isAdmin: false, status: 3 }, function (err, users) {
+    Account.find({ isAdmin: false, status: 3 }, function(err, users) {
         res.render('banning', {
             users
         });
@@ -118,8 +118,8 @@ Router.get('/banning', CheckLogin, (req, res) => {
 });
 //danh sách bị khóa vô thời hạn do đăng nhập sai
 Router.get('/bannedForever', CheckLogin, (req, res) => {
-    var sort = { date_register:-1 };
-    Account.find({ isAdmin: false,  inconstant_login: 1 }, function (err, users) {
+    var sort = { date_register: -1 };
+    Account.find({ isAdmin: false, inconstant_login: 1 }, function(err, users) {
         res.render('bannedForever', {
             users
         });
@@ -182,7 +182,7 @@ Router.post("/changePasswordadmin", changePassValidator, (req, res) => {
 });
 //Quản lý giao dịch - bắt đầu
 Router.get('/acceptTransaction', CheckLogin, (req, res) => {
-    Transaction.find({ status_transaction: 1 }, function (err, transaction) {
+    Transaction.find({ status_transaction: 1 }, function(err, transaction) {
         res.render('acceptTransaction', {
             transaction
         });
@@ -191,7 +191,7 @@ Router.get('/acceptTransaction', CheckLogin, (req, res) => {
 });
 // Xem thông tin chi tiết giao dịch
 Router.get('/acceptTransaction/:id', CheckLogin, (req, res) => {
-    Transaction.findById(req.params.id, function (err, transaction) {
+    Transaction.findById(req.params.id, function(err, transaction) {
         res.render('detailTransaction', {
             transaction
         });
@@ -199,106 +199,101 @@ Router.get('/acceptTransaction/:id', CheckLogin, (req, res) => {
 });
 // Đồng ý giao dịch và Hủy giao dịch
 Router.post('/acceptTransaction/:id', CheckLogin, (req, res) => {
-    let { status_transaction} = req.body;
+    let { status_transaction } = req.body;
     status_transaction = parseInt(status_transaction);
-    let id = req.params.id,time = new Date().toISOString(),nguoigui;
+    let id = req.params.id,
+        time = new Date().toISOString(),
+        nguoigui;
 
     if (status_transaction === 0) {
-        Transaction.findOne({_id:id}).then(transaction => {
-            Transaction.findByIdAndUpdate(id, { status_transaction },
-            ).then(() => {
-                if(transaction.kind===2)
-                {
-                    username=transaction.username
-                    Account.findOne({username}).then(account=>{
-                        Account.findByIdAndUpdate(account._id, { $inc: { account_balance: -transaction.money*1.05 } })
-                        .catch(err => {
-                            console.log(err);
-                        })
-                    })
-                }
-                else if(transaction.kind===1)
-                {
-                    username=transaction.username
-                    Account.findOne({username}).then(account=>{
-                        nguoigui = account.fullname;
-                    });
-                    //cap nhat so du nguoi nhan
-                    Account.findOne({_id:transaction.receiver_id}).then(account=>{
-                        if (transaction.nguoitra === 'nguoichuyentra') {
-                            Account.findByIdAndUpdate(account._id, { $inc: { account_balance: +transaction.money } }) 
-                            .catch(err => {
-                                console.log(err);
-                            })      
-                        } else if (transaction.nguoitra === 'nguoinhantra') {
-                            Account.findByIdAndUpdate(account._id, { $inc: { account_balance: +transaction.money*0.95 } })
+        Transaction.findOne({ _id: id }).then(transaction => {
+            Transaction.findByIdAndUpdate(id, { status_transaction }, ).then(() => {
+                if (transaction.kind === 2) {
+                    username = transaction.username
+                    Account.findOne({ username }).then(account => {
+                        Account.findByIdAndUpdate(account._id, { $inc: { account_balance: -transaction.money * 1.05 } })
                             .catch(err => {
                                 console.log(err);
                             })
-                        }
                     })
-                    .then(()=>{
-                        //mail thong bao so du nguoi gui
-                        Account.findOne({_id:transaction.receiver_id}).then(account=>{
-                            var mailOptions = {
-                                from: "ewallet.webnc@gmail.com",
-                                to: account.email,
-                                subject: "Giao dịch thành công E-Wallet",
-                                text: "Tài khoản của bạn " +
-                                    account.fullname +
-                                    "\nVừa nhận: " +
-                                    transaction.money + " VND." +
-                                    "\nPhí chuyển: " +
-                                    transaction.fee + " VND." +
-                                    "\nTừ: " +
-                                    nguoigui + " VND." +
-                                    "\nSố dư: " +
-                                    account.account_balance + " VND." +
-                                    "\nNgày giao dich: " + time + ".",
-                            };
-                            transporter.sendMail(mailOptions, function(error, info) {
-                                if (error) {
-                                    if (error) {
-                                        console.log(error);
-                                    }
-                                }
-                            });
+                } else if (transaction.kind === 1) {
+                    username = transaction.username
+                    Account.findOne({ username }).then(account => {
+                        nguoigui = account.fullname;
+                    });
+                    //cap nhat so du nguoi nhan
+                    Account.findOne({ _id: transaction.receiver_id }).then(account => {
+                            if (transaction.nguoitra === 'nguoichuyentra') {
+                                Account.findByIdAndUpdate(account._id, { $inc: { account_balance: +transaction.money } })
+                                    .catch(err => {
+                                        console.log(err);
+                                    })
+                            } else if (transaction.nguoitra === 'nguoinhantra') {
+                                Account.findByIdAndUpdate(account._id, { $inc: { account_balance: +transaction.money * 0.95 } })
+                                    .catch(err => {
+                                        console.log(err);
+                                    })
+                            }
                         })
-                    })
+                        .then(() => {
+                            //mail thong bao so du nguoi gui
+                            Account.findOne({ _id: transaction.receiver_id }).then(account => {
+                                var mailOptions = {
+                                    from: "ewallet.webnc@gmail.com",
+                                    to: account.email,
+                                    subject: "Giao dịch thành công E-Wallet",
+                                    text: "Tài khoản của bạn " +
+                                        account.fullname +
+                                        "\nVừa nhận: " +
+                                        transaction.money + " VND." +
+                                        "\nPhí chuyển: " +
+                                        transaction.fee + " VND." +
+                                        "\nTừ: " +
+                                        nguoigui + " VND." +
+                                        "\nSố dư: " +
+                                        account.account_balance + " VND." +
+                                        "\nNgày giao dich: " + time + ".",
+                                };
+                                transporter.sendMail(mailOptions, function(error, info) {
+                                    if (error) {
+                                        if (error) {
+                                            console.log(error);
+                                        }
+                                    }
+                                });
+                            })
+                        })
                 }
-            }
-            ).then(() => {
+            }).then(() => {
                 return res.redirect('/admin/acceptTransaction/' + req.params.id);
-            }).catch(err=>{
+            }).catch(err => {
                 console.log(err)
             })
         })
-    }
-    else if(status_transaction === 2)
-    {
-        Transaction.findOne({_id:id}).then(transaction => {
-            Transaction.findByIdAndUpdate(id, { status_transaction})
-            .then(()=>{
-                username=transaction.username
-                Account.findOne({username}).then(account=>{
-                    if (transaction.nguoitra === 'nguoichuyentra') {
-                        Account.findByIdAndUpdate(account._id, { $inc: { account_balance: +transaction.money*1.05 } }) 
-                        .catch(err => {
-                            console.log(err);
-                        })      
-                    } else if (transaction.nguoitra === 'nguoinhantra') {
-                        Account.findByIdAndUpdate(account._id, { $inc: { account_balance: +transaction.money } })
-                        .catch(err => {
-                            console.log(err);
-                        })
-                    }
+    } else if (status_transaction === 2) {
+        Transaction.findOne({ _id: id }).then(transaction => {
+            Transaction.findByIdAndUpdate(id, { status_transaction })
+                .then(() => {
+                    username = transaction.username
+                    Account.findOne({ username }).then(account => {
+                        if (transaction.nguoitra === 'nguoichuyentra') {
+                            Account.findByIdAndUpdate(account._id, { $inc: { account_balance: +transaction.money * 1.05 } })
+                                .catch(err => {
+                                    console.log(err);
+                                })
+                        } else if (transaction.nguoitra === 'nguoinhantra') {
+                            Account.findByIdAndUpdate(account._id, { $inc: { account_balance: +transaction.money } })
+                                .catch(err => {
+                                    console.log(err);
+                                })
+                        }
+                    })
                 })
-            })
-            .then(() => {
-                return res.redirect('/admin/acceptTransaction/');
-            }).catch(err=>{
-                console.log(err)
-            });
+                .then(() => {
+                    return res.redirect('/admin/acceptTransaction/');
+                }).catch(err => {
+                    console.log(err)
+                });
         });
     }
 });
